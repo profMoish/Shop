0bfe9cba1597e7dc47af1104b75214cf80ea20ab
mrMoish <@>
2026-06-02 15:06:44 -0300

    chore: wip

--- src/product.py
+++ src/product.py
@@ -1,11 +1,27 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, param: dict) -> 'Product':
+        # return Product(param['name'], param['description'], param['price'], param['quantity'])
+        return Product(**param)
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        if new_price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+        else:
+            self.__price = new_price
